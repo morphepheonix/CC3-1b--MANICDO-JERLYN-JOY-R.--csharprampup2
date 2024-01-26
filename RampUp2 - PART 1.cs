e0a41b2e8815f4ede9d1a9ef003ffc1d5abd943a
MORPHE <156663835+morphepheonix@users.noreply.github.com>
2024-01-26 11:04:42 +0800

    Update RampUp2 - PART 1.cs

--- RampUp2 - PART 1.cs
+++ RampUp2 - PART 1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace CSHARP_Ramp_Up_2_PART_1;
 
 class Program
@@ -5,22 +7,21 @@ class Program
     static void Main()
     {
         // PART 1 - POUNDS TO KILOGRAMS
-        Console.Write("Weight in Pounds (lbs): "); // USER INPUT VALUE
-        string weightInput = Console.ReadLine(); // USED TO READ NEXT LINE
+        Console.Write("Weight in Pounds (lbs): ");
+        string weightInput = Console.ReadLine(); // reads user's input [string] and stores [weightInput variable].
 
-        if (double.TryParse(weightInput, out double weightInPounds)) // it attempts to convert the user's input to a double.
-                                                                     // (tryparse) used when dealing with user input, where the input may not always be in a valid numeric format. 
-        {                                                            // If the conversion is successful, the code within the if block is executed.
-            double weightInKilograms = weightInPounds * 0.453592; // FORMULA FOR POUNDS TO KILOGRAMS // (*) USED ARITHMETIC OPERATOR 
+        if (double.TryParse(weightInput, out double weightInPounds)) // [tryparse] deals w/ the user input, where it may not always be in a valid numeric format. 
+        {
+            double weightInKilograms = weightInPounds * 0.453592;
             Console.WriteLine($"Weight converted to Kilograms (kg): {weightInKilograms:F2}"); // ($) Interpolated strings provide a concise and readable way to embed expressions within strings.
-                                                                                              // used when you want to create string by combining static text with the values of variables.
+                                                                                              // also when you want to create string by combining static text with the values of variables.
         }                                                                                     // :F2 formats the output to two decimal places.
         else
         {
             Console.WriteLine("INVALID INPUT. Please enter a valid weight."); // CHECKING IF THE USER INPUT WAS INCORRECT
         }
 
-        Console.WriteLine("========================================"); // USED FOR PARTITIONING CONVERSIONS.
+        Console.WriteLine("========================================"); // used for partioning parts
 
 
         // PART 1 - MILES TO KILOMETERS
@@ -29,12 +30,12 @@ class Program
 
         if (double.TryParse(lengthInput, out double lengthInMiles))
         {
-            double lengthInKilometers = lengthInMiles * 1.60934; // FORMULA FOR MILES TO KILOMETERS // (*) USED ARITHMETIC OPERATOR 
+            double lengthInKilometers = lengthInMiles * 1.60934;
             Console.WriteLine($"Length in Kilometers (km): {lengthInKilometers:F2}");
         }
         else
         {
-            Console.WriteLine("INVALID INPUT. Please enter a valid len."); // CHECKING IF THE USER INPUT WAS INCORRECT
+            Console.WriteLine("INVALID INPUT. Please enter a valid len.");
         }
 
 
@@ -47,12 +48,12 @@ class Program
 
         if (double.TryParse(tempInput, out double tempInFahrenheit))
         {
-            double tempInCelsius = (tempInFahrenheit - 32) * 5 / 9; // FORMULA FOR FAHRENHEIT TO CELSIUS // (* & /) USED ARITHMETIC OPERATOR 
+            double tempInCelsius = (tempInFahrenheit - 32) * 5 / 9;
             Console.WriteLine($"Temperature in Celsius (°C): {tempInCelsius:F2}");
         }
         else
         {
-            Console.WriteLine("INVALID INPUT. Please enter a valid temparature."); // CHECKING IF THE USER INPUT WAS INCORRECT
+            Console.WriteLine("INVALID INPUT. Please enter a valid temparature.");
         }
 
         Console.WriteLine("========================================");
@@ -61,43 +62,42 @@ class Program
         // PART 1 - STUDENT AGE's AVERAGE
 
 
-        int numberOfStudents = 10;
-        int sumOfAges = 0;
+        int numberOfStudents = 10;  //set to 10, indicating the number of students for which you want to collect ages.
+        int sumOfAges = 0;          //initialized to 0, and it will be used to calculate the total sum of valid ages.
 
-        for (int i = 1; i <= numberOfStudents; i++) // The program uses a for loop to iterate through 10 students 
+        for (int i = 1; i <= numberOfStudents; i++) // used to iterate through each student, from 1 to the specified numberOfStudents.
         {
-            Console.Write($"Age of Student {i}: ");
+            Console.Write($"Age of Student {i}: "); // ask user to enter the age of each student.
             string ageInput = Console.ReadLine();
 
-            if (int.TryParse(ageInput, out int age) && age >= 0) // [int.TryParse(ageInput, out int age)] attempts to convert the user's input to an integer.
-                                                                 // If the conversion is successful (TryParse returns true) and the age is greater than or equal to 0, the age is considered valid.
-            {                                                    // (&&) Logical and: Returns True if both statements are true
+            if (int.TryParse(ageInput, out int age) && age >= 0) // [int.TryParse] converts user's input to an integer (age).
+                                                                 // If successful ([&&] - returns true) and the age is greater than or equal to 0, the age is considered valid.
+            {
                 sumOfAges += age; // if the age input is valid, it is added to the running total of ages (sumOfAges)
             }
             else
             {
-                Console.WriteLine("INVALLID INPUT. Please enter a valid age."); // CHECKING IF THE USER INPUT WAS INCORRECT
+                Console.WriteLine("INVALLID INPUT. Please enter a valid age.");
             }
 
+            double averageAge = (double)sumOfAges / numberOfStudents;
+            Console.WriteLine($"The average age of the students is: {averageAge:F1}"); // :F1 formats the output to one decimal places.
+                                                                                       // ($) Interpolated strings provide a concise and readable way to embed expressions within strings.
+            Console.WriteLine("========================================");
+
+            // PART 1 - SHORT STORY W/ 5 CHARACTERS
+            // CHARACTER INFO.
+            string Artifact = "ice-crafted weapons"; // represents various aspects of the characters and their attributes
+            string Protagonist = "Lorna Frostblade";
+            string Antagonist = "Shadowblade";
+            string Weaponry = "icy blades";
+            string Ability = "flash";
+
+            // This string used an interpolated string ($"...") where variables are injected into the string using curly braces {}.
+            // - The values of those variables will replace the placeholders in the string.
+            string story = $"In the desolate wastelands, Captain {Protagonist}, a relentless bounty hunter with {Artifact}, pursued her nemesis, the elusive rogue known only as {Antagonist}. Their bitter rivalry echoed through the abandoned ruins as Captain {Protagonist} closed in, determined to bring justice to the lands. With a {Ability} of {Weaponry}, she cornered {Antagonist}, ending the tale of their enmity in a frozen showdown beneath the pale moonlight.";
+            Console.WriteLine(story);
+            Console.Read(); //used to keep the console window open until a key is pressed, allowing the user to read the story before the program exits.
         }
-
-
-        double averageAge = (double)sumOfAges / numberOfStudents; // FORMULA FOR GETTING THE STUDENTS AGE AVERAGE // (+ & /) USED ARITHMETIC OPERATOR 
-        Console.WriteLine($"The average age of the students is: {averageAge:F1}");
-
-        Console.WriteLine("========================================");
-
-        // PART 1 - SHORT STORY W/ 5 CHARACTERS
-        // CHARACTER INFO.
-        string Artifact = "ice-crafted weapons"; // Artifact, Protagonist, Antagonist, Weaponry, and Ability are strings representing various aspects of the characters and their attributes
-        string Protagonist = "Lorna Frostblade";
-        string Antagonist = "Shadowblade";
-        string Weaponry = "icy blades";
-        string Ability = "flash";
-
-        //The story string is composed using an interpolated string ($@"...") where variables are injected into the string using curly braces {}.
-        string story = $@"In the desolate wastelands, Captain {Protagonist}, a relentless bounty hunter with {Artifact}, pursued her nemesis, the elusive rogue known only as {Antagonist}. Their bitter rivalry echoed through the abandoned ruins as Captain {Protagonist} closed in, determined to bring justice to the lands. With a {Ability} of {Weaponry}, she cornered {Antagonist}, ending the tale of their enmity in a frozen showdown beneath the pale moonlight.";
-        Console.WriteLine(story);
-        Console.Read(); //used to keep the console window open until a key is pressed, allowing the user to read the story before the program exits.
     }
 }
